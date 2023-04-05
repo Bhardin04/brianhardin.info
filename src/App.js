d20c41619bd4ae718b7e4da460da00d2c3d532e7
bhardin <brian.hardin@icloud.com>
2023-04-05 19:38:49 +0000

    Still testing git changes and process with vscode server

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          I think I got this to work!
+          I think these changes are flowing!
         </p>
         <a
           className="App-link"
